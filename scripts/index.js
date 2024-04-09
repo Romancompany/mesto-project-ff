4b3d8110396de26fbd630b6850827b24c8658737
Roman Nikolaev <romancompany@mail.ru>
2024-04-09 20:15:56 +0300

    ПР-5

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,32 @@
 // @todo: Темплейт карточки
+const templateCard = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const cardContainer = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function createCard(element,delCard) {
+    const tmpCard = templateCard.cloneNode(true);
+    const imgCard = tmpCard.querySelector('.card__image');
+    const titleCard = tmpCard.querySelector('.card__title');
+    const deleteButtonCard = tmpCard.querySelector('.card__delete-button');
+
+    imgCard.src = element.link;
+    imgCard.alt = element.name;
+    titleCard.textContent = element.name;
+    deleteButtonCard.addEventListener('click', function(evt) { delCard(evt.target); } );
+
+    return tmpCard;
+}
 
 // @todo: Функция удаления карточки
+function deleteCard(deleteButton) {
+    deleteButton.parentElement.remove();
+}
 
 // @todo: Вывести карточки на страницу
+function fillCards(Cards) {
+    Cards.forEach(element => { cardContainer.append(createCard(element,deleteCard)); });
+}
+
+fillCards(initialCards);
